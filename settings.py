a198cfd0dca836ec81395ef47545746cc7ef5408
Ivan Khomyuk <mr.homyuk@mail.ru>
2024-03-29 22:41:20 +0900

    Some fix.

--- settings.py
+++ settings.py
@@ -1,12 +1,10 @@
 import os
 
-from dotenv import load_dotenv
-
-load_dotenv()
-
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        'DATABASE_URI', default='sqlite:///db.sqlite3'
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.getenv('SECRET_KEY')
+    SECRET_KEY = os.getenv('SECRET_KEY', default='abcdef')
     JSON_SORT_KEYS = False
